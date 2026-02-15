6cf2022c5cfc212bf6b040f2ddace144ab977c61
KrisvHeij <49883345+KrisvHeij@users.noreply.github.com>
2026-02-15 22:18:40 +0100

    Updated

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const textInputs = document.querySelectorAll(".input");
+const radioInputs = document.querySelectorAll(".input-radio");
+const checkInpout = document.querySelector(".input-checkbox");
 const submitBtn = document.querySelector("button");
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  for (let i = 0; i < 4; i++) {
+
+  for (let i = 0; i < textInputs.length; i++) {
     console.log(textInputs[i].value);
   }
+
+  for (let i = 0; i < radioInputs.length; i++) {
+    console.log(radioInputs[i].value);
+  }
+  
+  console.log(`${checkInpout.checked ? "check" : "not checked"}`);
 })
