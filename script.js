bd24fa79267b9ad7f83e3f6423788f5207b7f24c
KrisvHeij <49883345+KrisvHeij@users.noreply.github.com>
2026-02-18 20:16:28 +0100

    Updated html & handleFirstName()

--- script.js
+++ script.js
@@ -3,14 +3,24 @@ const radioInputs = document.querySelectorAll(".input-radio");
 const checkInpout = document.querySelector(".input-checkbox");
 const submitBtn = document.querySelector("button");
 
+
 function handleFirstName() {
   const firstName = document.getElementById("firstName");
- 
-  if (firstName.value === "") {
-    
+  const error = document.querySelector(".error-first-name");
+  const regex = /^[a-zA-Z]+$/;
+  
+  if (firstName.value === "" || !regex.test(firstName.value)) {
+    error.removeAttribute("hidden");
+    firstName.classList.add("error-border");
+    console.log("false");
+  } else {
+    console.log("true")
   }
 }
 
+function handleLastName() {
+  
+}
 
 
 
